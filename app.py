a019d7a6107b975d197e35a1714ad5bd6724a17e
Jen-Dean <missjenniferdean@gmail.com>
2020-11-08 18:39:01 -0500

    finished main portion of HW

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-session = Session(engine)
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -35,11 +33,15 @@ def homepage():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
+    session = Session(engine)
+
     previous_date = dt.datetime(2016, 8, 23)
 
     prcp = session.query(func.avg(Measurement.prcp), Measurement.date).\
     filter(Measurement.date > previous_date).\
     group_by(Measurement.date).all()
+
+    session.close()
 
     prcp_list = []
 
@@ -52,44 +54,90 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
+
+    session = Session(engine)
+
     station_list = session.query(Station.station).all()
+
+    session.close()
+
     return (
         jsonify(station_list)
     )
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+
     previous_date = dt.datetime(2016, 8, 23)
+
+    session = Session(engine)
+
     tobs = session.query(Measurement.tobs).\
         filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date > previous_date).all()
+
+    session.close()
+    
     return (
         jsonify(tobs)
     )
 
 @app.route("/api/v1.0/<start>")
 def start_date_only(start):
-    date = start.datetime(start)
+    date = start
+
+    session = Session(engine)
+
     min_tobs = session.query(func.min(Measurement.tobs)).\
-        filter(Measurement.date <= date).all()
+        filter(Measurement.date >= date).all()
     avg_tobs = session.query(func.avg(Measurement.tobs)).\
-        filter(Measurement.date <= date).all()
+        filter(Measurement.date >= date).all()
     max_tobs = session.query(func.max(Measurement.tobs)).\
-        filter(Measurement.date <= date).all()
+        filter(Measurement.date >= date).all()
+
+    session.close()
 
     return (
-        f"The Minimum Tempurature on {date} was {min_tobs}</br>"
-        f"The Average Tempurature on {date} was {avg_tobs}</br>"
-        f"The Maximum Tempurature on {date} was {max_tobs}</br>"
+
+        jsonify({
+        "min_tob": min_tobs,
+        "avg_tob": avg_tobs,
+        "max_tob": max_tobs})
+
+        #f"The Minimum Tempurature on {date} was {min_tobs[0]}</br>"
+        #f"The Average Tempurature on {date} was {avg_tobs[0]}</br>"
+        #f"The Maximum Tempurature on {date} was {max_tobs[0]}</br>"
     )
 
 @app.route("/api/v1.0/<start>/<end>")
-def end():
+def end(start, end):
+    date_s = start
+    date_e = end
+
+    session = Session(engine)
+
+    min_tobs = session.query(func.min(Measurement.tobs)).\
+        filter(Measurement.date <= date_e).\
+        filter(Measurement.date >= date_s).all()
+    avg_tobs = session.query(func.avg(Measurement.tobs)).\
+        filter(Measurement.date <= date_e).\
+        filter(Measurement.date >= date_s).all()
+    max_tobs = session.query(func.max(Measurement.tobs)).\
+        filter(Measurement.date <= date_e).\
+        filter(Measurement.date >= date_s).all()
+
+    session.close()
+
     return (
-        "hello"
+        jsonify({
+        "min_tob": min_tobs,
+        "avg_tob": avg_tobs,
+        "max_tob": max_tobs})
+
+        #f"The Minimum Tempurature between {date_s} and {date_e} was {min_tobs[0]}</br>"
+        #f"The Average Tempurature between {date_s} and {date_e} was {avg_tobs[0]}</br>"
+        #f"The Maximum Tempurature between {date_s} and {date_e} was {max_tobs[0]}</br>"
     )
-
-
 
 
 
