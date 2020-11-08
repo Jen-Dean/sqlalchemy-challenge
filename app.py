743f1e88ad016d6201f47020512a806fdd97274e
Jen-Dean <missjenniferdean@gmail.com>
2020-11-08 17:56:43 -0500

    updates

--- app.py
+++ app.py
@@ -23,7 +23,13 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     return (
-        "hello"
+        f"hello! Welcome to my SQLAlchemy Challenge Website!<br>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
